2f4a9dc7c01d0760035563f89b17d58539220f76
Kailash Kumar <202051093@iiitvadodara.ac.in>
2025-12-30 17:06:09 +0530

    coins overview and trending part

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
-const Page = () => {
+import CoinOverview from "@/components/home/CoinOverview"
+import { CoinOverviewFallback, TrendingCoinsFallback } from "@/components/home/fallback"
+import TrendingCoins from "@/components/home/TrendingCoins"
+import { Suspense } from "react"
+
+const Page = async () => {
   return (
-    <></>
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
   )
 }
 
